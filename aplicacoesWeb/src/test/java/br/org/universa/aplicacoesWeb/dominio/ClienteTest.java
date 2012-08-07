6674e4ab3bb4762a6098a8035a801ce8897bb74a
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2012-08-07 12:08:08 -0300

    Implementação da classe de teste de cliente; Correções relacionadas com o Maven;

--- aplicacoesWeb/src/test/java/br/org/universa/aplicacoesWeb/dominio/ClienteTest.java
+++ aplicacoesWeb/src/test/java/br/org/universa/aplicacoesWeb/dominio/ClienteTest.java
@@ -1,14 +1,42 @@
 package br.org.universa.aplicacoesWeb.dominio;
 
-import static org.junit.Assert.fail;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 public class ClienteTest {
 
 	@Test
-	public void testGetId() {
-		fail("Not yet implemented");
+	public void metodosGetsESetsDevemFuncionar() {
+		// Cenário:
+		Cliente c = new Cliente();
+
+		c.setBairro("Norte");
+		c.setCnpj("99999999999/0001");
+		c.setContato("PH");
+		c.setEndereco("Rua da Alegria");
+		c.setEstado("DF");
+		c.setHomepage("www.google.com.br");
+		c.setId(0);
+		c.setMunicipio("Águas Claras");
+		c.setQuantidadeEmpregados(2000);
+		c.setTelefone("33218181");
+
+		// Ação:
+
+		// Validações:
+
+		Assert.assertEquals("Norte", c.getBairro());
+		Assert.assertEquals("99999999999/0001", c.getCnpj());
+		Assert.assertEquals("PH", c.getContato());
+		Assert.assertEquals("Rua da Alegria", c.getEndereco());
+		Assert.assertEquals("DF", c.getEstado());
+		Assert.assertEquals("www.google.com.br", c.getHomepage());
+		Assert.assertEquals(0, c.getId());
+		Assert.assertEquals("Águas Claras", c.getMunicipio());
+		Assert.assertEquals(2000, c.getQuantidadeEmpregados());
+		Assert.assertEquals("33218181", c.getTelefone());
+
 	}
 
 }
